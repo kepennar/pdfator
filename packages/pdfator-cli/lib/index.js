ae15e77d830aaf71b48a4c6f512bd52d8400e381
Kevin Pennarun <kevin.pennarun@gmail.com>
2018-08-26 10:40:51 +0200

    fix cli

--- packages/pdfator-cli/lib/index.js
+++ packages/pdfator-cli/lib/index.js
@@ -1,29 +1,31 @@
-const clear = require('clear');
-const chalk = require('chalk');
-const figlet = require('figlet');
-const { Spinner } = require('clui');
-const { convert } = require('@pdfator/core');
+#!/usr/bin/env node
 
-const { askConvertionConfig } = require('./inquirer');
+const clear = require ('clear');
+const chalk = require ('chalk');
+const figlet = require ('figlet');
+const {Spinner} = require ('clui');
+const {convert} = require ('@pdfator/core');
 
-clear();
-console.log(
-  chalk.yellow(figlet.textSync('PdfAtor', { horizontalLayout: 'full' }))
+const {askConvertionConfig} = require ('./inquirer');
+
+clear ();
+console.log (
+  chalk.yellow (figlet.textSync ('PdfAtor', {horizontalLayout: 'full'}))
 );
 
 const run = async () => {
   try {
-    const config = await askConvertionConfig();
-    const loader = new Spinner(`Converting "${config.url}" please wait...`);
-    loader.start();
-    await convert({ ...config, flushToDisk: true });
-    loader.stop();
+    const config = await askConvertionConfig ();
+    const loader = new Spinner (`Converting "${config.url}" please wait...`);
+    loader.start ();
+    await convert ({...config, flushToDisk: true});
+    loader.stop ();
   } catch (error) {
-    clear();
-    console.log(
-      chalk.red(figlet.textSync('ERROR', { horizontalLayout: 'full' }))
+    clear ();
+    console.log (
+      chalk.red (figlet.textSync ('ERROR', {horizontalLayout: 'full'}))
     );
   }
 };
 
-run();
+run ();
